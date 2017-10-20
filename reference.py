6e008f13e038db822453f52ddeef1c30cc1723d8
Chris Turner <cat@199tech.net>
2017-10-20 14:25:32 -0500

    reference.py: djify. currently included classes are loading.

--- reference.py
+++ reference.py
@@ -1,35 +1,72 @@
 # <class 'misc.models.OrderedUser'>
 # <class 'django.contrib.auth.models.User'>
+# <class 'misc.models.BrainLocation'>
+
+import datajoint as dj
+
+schema = dj.schema(dj.config['FIXME'], locals())
+
 
 @schema
-class OrderedUser(dj.Manual):
-    definition="""
-    id:    type_unknown <class 'django.db.models.fields.AutoField'> # ID
+class User(dj.Manual):
+
+    definition = """
+    username:		varchar(255)	# username
     ---
-    password:    varchar(255) # password
-    last_login:    datetime # last login
-    is_superuser:    boolean # superuser status
-    username:    varchar(255) # username
-    first_name:    varchar(255) # first name
-    last_name:    varchar(255) # last name
-    email:    varchar(255) # email address
-    is_staff:    boolean # staff status
-    is_active:    boolean # active
-    date_joined:    datetime # date joined
-    groups:    type_unknown <class 'django.db.models.fields.related.ManyToManyField'> # groups
-    user_permissions:    type_unknown <class 'django.db.models.fields.related.ManyToManyField'> # user permissions
+    password:		varchar(255)	# password
+    email:		varchar(255)	# email address
+    last_login:		datetime	# last login
+    first_name:		varchar(255)	# first name
+    last_name:		varchar(255)	# last name
+    date_joined:	datetime	# date joined
+    is_active:		boolean		# active
+    is_staff:		boolean		# staff status
+    is_superuser:	boolean		# superuser status
     """
 
 
-# <class 'misc.models.BrainLocation'>
+@schema
+class UserGroup(dj.Manual):
+    TODO = True
+    definition = """
+    groupname:		varchar(255)	# groupname
+    """
+
+    class UserGroupMembership(dj.Part):
+
+        definition = """
+        -> UserGroup
+        -> User
+        """
+
+
+@schema
+class PermissionType(dj.Lookup):
+
+    definition = """
+    permission: varchar(30)
+    """
+    contents = zip(['p1', 'p2', 'p3'])
+
+
+@schema
+class UserPermission(dj.Lookup):
+
+    definition = """
+    -> User
+    -> PermissionType
+    """
+
+
 @schema
 class BrainLocation(dj.Manual):
-    definition="""
-    id:    char(32) # id
+
+    definition = """
+    id:				char(32)	# id
     ---
-    json:    varchar(255) # json
-    name:    varchar(255) # name
-    stereotaxic_coordinates:    longblob # stereotaxic coordinates
-    description:    varchar(255) # description
-    allen_location_ontology:    varchar(255) # allen location ontology
+    json:			varchar(255)	# json
+    name:			varchar(255)	# name
+    stereotaxic_coordinates:	longblob	# stereotaxic coordinates
+    description:		varchar(255)	# description
+    allen_location_ontology:    varchar(255)	# allen location ontology
     """
