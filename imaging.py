fb37f091009055a671dccb679f12b78256f81b37
Chris Turner <cat@199tech.net>
2017-10-25 18:48:57 -0500

    imaging.py: initial datajoint-style implementation

--- imaging.py
+++ imaging.py
@@ -1,3 +1,107 @@
 import datajoint as dj
 
+import reference
+import equipment
+import actions
+
 schema = dj.schema(dj.config['names.%s' % __name__], locals())
+
+
+@schema
+class SVDCompressedMovie(dj.Manual):
+    '''
+    Note:
+    eigenframes: (nSVs*nY*nX)
+    timecourses: (nSamples*nSVs)
+    '''
+    definition = """
+    -> actions.Session
+    svd_movie_id:               int             # svd movie id
+    ---
+    -> reference.User
+    create_date:                datetime        # create date
+    eigenframes:                longblob        # svd eigenframes
+    timecourses:                longblob        # svd timecourses
+    """
+
+
+@schema
+class WidefieldImaging(dj.Manual):
+    # <class 'imaging.models.WidefieldImaging'>
+    # TODO: Reference Stack
+    # TODO: rename imported fkeys; awaiting dj #300
+    # TODO: nullable CoordinateTransformation
+    # TODO: nullable SVDCompressedMovie
+    # TODO: nullable LightSource
+    definition = """
+    -> actions.Session
+    ---
+    -> reference.User
+    create_date:                datetime        # create date
+    start_time:			datetime	# nominal start time
+    end_time:			datetime	# nominal end time
+    imaging_indicator:		varchar(255)	# imaging indicator
+    preprocessing:		varchar(255)	# preprocessing
+    description:		varchar(255)	# description
+    excitation_wavelength:	float		# excitation nominal wavelength
+    recording_wavelength:	float		# recording nominal wavelength
+    recording_device:		varchar(255)	# recording device
+    raw_frames=NULL:            longblob        # n*w*h*c video
+    -> reference.CoordinateTransformation
+    -> SVDCompressedMovie
+    -> equipment.Appliance.LightSource
+    """
+
+
+@schema
+class TwoPhotonImaging(dj.Manual):
+    # <class 'imaging.models.TwoPhotonImaging'>
+    # TODO: Reference Stack
+    # TODO: rename imported fkeys; awaiting dj #300
+    # TODO: nullable CoordinateTransformation
+    # TODO: nullable SVDCompressedMovie
+    definition = """
+    -> actions.Session
+    ---
+    -> reference.User
+    create_date:                datetime        # create date
+    start_time:			datetime	# nominal start time
+    end_time:			datetime	# nominal end time
+    description:		varchar(255)	# description
+    excitation_wavelength:	float		# excitation nominal wavelength
+    recording_wavelength:	float		# recording nominal wavelength
+    raw_frames=NULL:            longblob        # n*w*h*c video
+    -> reference.CoordinateTransformation
+    -> SVDCompressedMovie
+    """
+
+
+@schema
+class ROIDetection(dj.Manual):
+    # <class 'imaging.models.TwoPhotonImaging'>
+    definition = """
+    -> actions.Session
+    ---
+    -> reference.User
+    create_date:                datetime        # create date
+    start_time:			datetime	# nominal start time
+    end_time:			datetime	# nominal end time
+    preprocessing:		varchar(255)	# preprocessing
+    """
+
+    class ROI(dj.Part):
+        # <class 'imaging.models.ROI'>
+        definition = """
+        -> ROIDetection
+        roi_id:                 int             # ROI ID
+        ---
+        roi_type:               varchar(255)    # roi type
+        optogenetic_response:   varchar(255)    # optogenetic response
+        putative_cell_type:     varchar(255)    # putative cell type
+        estimated_layer:        varchar(255)    # estimated layer
+        -> TwoPhotonImaging
+        raw_fluorescence:       longblob        # nTxNROI
+        resting_fluorescence:   longblob        # nTxNROI
+        masks:                  longblob        # (nYxnX) masks
+        plane:                  integer         # discovery plane
+        """
